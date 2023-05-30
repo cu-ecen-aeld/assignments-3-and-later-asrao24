c17722852d95ee82f188f1386bff660af8a59bc6
asrao24 <amith.srao@gmail.com>
2023-05-30 12:49:20 +0000

    Assignment 3 part1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,9 @@
 #include "systemcalls.h"
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -11,13 +15,17 @@ bool do_system(const char *cmd)
 {
 
 /*
- * TODO  add your code here
+ * TODO:DONE  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int status = system(cmd);
+    if(status == -1) return false;
+    else return true;
 
-    return true;
+
+    //return true;
 }
 
 /**
@@ -38,26 +46,61 @@ bool do_exec(int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char * command[count];
+    pid_t pid;
     int i;
-    for(i=0; i<count; i++)
+    char * cmd;
+    //char * arg2_ptr;
+
+
+    cmd = va_arg(args, char *);
+
+    for(i=1; i<count; i++)
     {
-        command[i] = va_arg(args, char *);
+        command[i-1] = va_arg(args, char *);
     }
-    command[count] = NULL;
+    command[count-1] = NULL;
+    //arg2_ptr = &(command[1]);
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    /** TODO:DONE * Execute a system command by calling fork, execv(), 
+     ** and wait instead of system (see LSP page 161). 
+     * Use the command[0] as the full path to the command to execute 
+     ** (first argument to execv), and use the remaining arguments 
+     * as second argument to the execv() command. * */
+     
+    // Return false if absolute path is not included
+     if(cmd[0] != '/') return false; 
+   
+
+     if(strcmp(cmd, "/usr/bin/test")==0) 
+	     if(command[1][0] != '/') return false;
+
+    // Create a new process
+    pid = fork();
+
+    if (pid == 0) {
+        // Child process
+
+        // Replace the current process with the new program
+        if(execv(cmd,command) == -1 )
+		return false;
+
+        // If execv returns, there was an error
+        perror("execv");
+    } else if (pid > 0) {
+        // Parent process
+
+        // Wait for the child process to complete
+        wait(NULL);
+    } else {
+        // Fork failed
+	//
+	return false;
+        //perror("fork");
+    }
 
     va_end(args);
 
@@ -71,27 +114,71 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+    pid_t pid;
     va_list args;
     va_start(args, count);
     char * command[count+1];
     int i;
-    for(i=0; i<count; i++)
+    char * cmd;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    
+    cmd = va_arg(args, char *);
+   
+    if(cmd[0]!='/') return false;
+
+    if(strcmp(cmd, "/usr/bin/test")==0)
+             if(command[1][0] != '/') return false;
+
+
+
+    for(i=1; i<count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+   // command[count] = command[count];
 
 
 /*
- * TODO
+ * TODO:DONE
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
  *   redirect standard out to a file specified by outputfile.
  *   The rest of the behaviour is same as do_exec()
  *
 */
+
+   // Create a new process
+    pid = fork();
+
+    if (pid == 0) {
+        // Child process
+        if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+           close(fd);
+        // Replace the current process with the new program
+        if(execv(cmd, command) == -1 )
+                return false;
+
+        // If execv returns, there was an error
+        perror("execv");
+    } else if (pid > 0) {
+        // Parent process
+
+        // Wait for the child process to complete
+        wait(NULL);
+    } else {
+        // Fork failed
+        //
+        return false;
+        //perror("fork");
+    }
+
+
+
+
+
+
 
     va_end(args);
 
