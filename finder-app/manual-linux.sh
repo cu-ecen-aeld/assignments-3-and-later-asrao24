c8eff63b964cbbe9f9ce7152db58811923d7b439
asrao24 <amith.srao@gmail.com>
2023-07-07 15:25:26 +0000

    next try

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -146,10 +146,10 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 #${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | awk '{print $4}' | tr -d ']' | xargs -I '{}' sudo cp -L '{}' "${OUTDIR}/rootfs/lib"
 #${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | awk '{print $1}' | tr -d ']' | xargs -I '{}' sudo cp -L '{}' "${OUTDIR}/rootfs/lib64"
 
-sudo cp -L ~/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 "${OUTDIR}/rootfs/lib"
-sudo cp -L ~/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 "${OUTDIR}/rootfs/lib64"
-sudo cp -L ~/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 "${OUTDIR}/rootfs/lib64"
-sudo cp -L ~/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 "${OUTDIR}/rootfs/lib64"
+sudo cp -L /home/amith_srao/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 "${OUTDIR}/rootfs/lib"
+sudo cp -L /home/amith_srao/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 "${OUTDIR}/rootfs/lib64"
+sudo cp -L /home/amith_srao/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 "${OUTDIR}/rootfs/lib64"
+sudo cp -L /home/amith_srao/aarc64_cross_compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 "${OUTDIR}/rootfs/lib64"
 
 # TODO: Make device nodes
 sudo mknod -m 666 dev/null c 1 3
